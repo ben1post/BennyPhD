7cfbdfc5806e2685eb52e2be906fc076d7732610
Benjamin Post <benjaminpost@ymail.com>
2020-02-18 15:06:53 -0800

    Delete OSM2020PosterToDo.mm

--- OSM2020_poster/OSM2020PosterToDo.mm
+++
@@ -1,4 +0,0 @@
-<map version="1.1.0">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1579219751618" ID="ID_1933823119" MODIFIED="1579219925564" TEXT="&quot;Modelling phytoplankton community changes in the CARIACO basin, Venezuela&quot;"/>
-</map>
