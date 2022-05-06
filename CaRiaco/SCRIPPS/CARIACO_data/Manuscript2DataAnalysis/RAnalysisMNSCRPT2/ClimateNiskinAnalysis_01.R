f813ec91d060d0184142355d22948c7306d00d90
Benjamin Post <benjaminpost@ymail.com>
2022-05-06 11:17:10 +0200

    start working on wind data from CCMP

--- CaRiaco/SCRIPPS/CARIACO_data/Manuscript2DataAnalysis/RAnalysisMNSCRPT2/ClimateNiskinAnalysis_01.R
+++ CaRiaco/SCRIPPS/CARIACO_data/Manuscript2DataAnalysis/RAnalysisMNSCRPT2/ClimateNiskinAnalysis_01.R
@@ -8,14 +8,24 @@ library(lubridate)
 options(stringsAsFactors = FALSE)
 
 # Machine Learning Analysis Packages
-library(dismo)
-library(gbm)
+# library(dismo)
+# library(gbm)
 
-install.packages( pkgs = "terra"
-                  , method = "curl"
-                  , configure.args = c(
-                    "--with-gdal-config=/Library/Frameworks/GDAL.framework/Programs/gdal-config"
-                    , "--with-proj-include=/bpo/home/bin/proj4/include"
-                    , "--with-proj-lib=/bpo/home/bin/proj4/lib"
-                  ) 
-)
+data <- read.csv("../DATA/January/Combined_CARIACO_data_v6.csv")
+
+gmldat <- data %>%
+  select('mcc', 'mwp', 'sp', 'cdir', 'sst','tp', 'Satellite_chla') %>%
+  drop_na()
+
+gmldat2 <- data %>%
+  select('mcc', 'mwp', 'sp', 'cdir', 'sst','tp', 'Chlorophyll_35m') %>%
+  drop_na()
+
+library(fitdistrplus)
+descdist(gmldat$Satellite_chla, discrete = FALSE)
+
+descdist(log(gmldat$Satellite_chla), discrete = FALSE)
+
+descdist(gmldat2$Chlorophyll_35m, discrete = FALSE)
+
+descdist(log(gmldat2$Chlorophyll_35m), discrete = FALSE)
